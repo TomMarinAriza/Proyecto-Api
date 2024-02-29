18db2c3025ee94df20d30c9fca9b80f7805563dc
TomMarinAriza <t.marin@utp.edu.co>
2024-02-28 20:31:51 -0500

    Update

--- API/API.py
+++ API/API.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from sodapy import Socrata
 
-def Get_data (limite_registros , nombre_departamento):
-    client = Socrata("www.datos.gov.co", None)
-    results = client.get("gt2j-8ykr", limit=limite_registros, departamento_nom=nombre_departamento)
+API = "www.datos.gov.co"
+API_TOKEN = "gt2j-8ykr"
+
+def Get_data (limite_registros , nombre_departamento ):
+    client = Socrata(API, None)
+    results = client.get(API_TOKEN, limit=limite_registros, departamento_nom=nombre_departamento )
     results_df = pd.DataFrame.from_records(results)
     return results_df
 
--- UI/Interface.py
+++ UI/Interface.py
@@ -1,22 +1,54 @@
 import API.API
 
-
-print("Bienvenido a su programa para hacer la busqueda de casos positivos a covid\n")
-busqueda_departamento = input("Digite el nombre del departamento \n")
-control = True
-
-while (control):
-    limite_registros = input("Digite el limite de registros a visualizar ")
-    if (int(limite_registros) >=100):
-        print("Valor no valido\n")
-    else:
-        control =False
+departamentos = {"AMAZONAS","ANTIOQUIA","ARAUCA","ATLANTICO",
+                 "BOLIVAR" , "BOYACA" , "CALDAS" ,"CAQUETA",
+                 "CASANARE" ,"CAUCA" ,"CESAR" , "CHOCO",
+                 "CORDOBA" , "CUNDINAMARCA" , "GUANIA",
+                 "GUAVIARE" , "HUILA", "GUAJIRA" , "MAGDALENA",
+                 "META" , "NARIÑO" ,  "SANTANDER", "PUTUMAYO",
+                 "QUINDIO" , "RISARALDA" , "NORTE SANTANDER" , "SUCRE",
+                 "TOLIMA" , "VALLE" , "VAUPES" , "VICHADA"
+                 }
 
 
-table = API.API.Get_data(int(limite_registros),busqueda_departamento)
+def Menssages ():
+    print("Bienvenido a su programa para hacer la busqueda de casos positivos a covid\n")
+def Control_deparment(lista_departamentos):
+    control = True
+    while (control):
+        busqueda_departamento = input("Digite el nombre del departamento \n")
+        if (busqueda_departamento not in lista_departamentos):
+            print("Departamento no valido digitelo otra vez \n")
+        else:
+            control = False
+    return busqueda_departamento
 
-columnas_deseadas = ["ciudad_municipio_nom","departamento_nom","edad","tipo_recuperacion","estado"]
+def Control_register():
+    control = True
+    while (control):
+        limite_registros = input("Digite el limite de registros a visualizar ")
+        if (int(limite_registros) >= 100):
+            print("Valor no valido\n")
+        else:
+            control = False
+    return limite_registros
 
-table = table[columnas_deseadas]
-print(table)
+def Print_Table (limite_registros,busqueda_departamento):
 
+    table = API.API.Get_data(int(limite_registros), busqueda_departamento)
+
+    columnas_deseadas = ["ciudad_municipio_nom", "departamento_nom", "edad", "tipo_recuperacion", "estado"]
+
+    table = table[columnas_deseadas]
+    print(table)
+
+def Cicle ():
+    controlator = True
+
+    while (controlator):
+        Menssages()
+        Print_Table(Control_register(),Control_deparment(departamentos))
+        answer = input("Desea realizar otra consulta S/N  ")
+        if answer == "N":
+            controlator = False
+            print("Gracias por usar el programa")
